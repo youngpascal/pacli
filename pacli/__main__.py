018f6c659f0977c8ec13f984087de52b50dee6c5
youngpascal <josh.wilburn@protonmail.com>
2018-05-09 20:06:12 -0500

    adding basic functionality

--- pacli/__main__.py
+++ pacli/__main__.py
@@ -2,7 +2,8 @@ import fire
 import pypeerassets as pa
 from pacli.provider import provider
 from pacli.config import Settings
-
+from terminaltables import AsciiTable
+from utils import print_table
 
 class Deck:
 
@@ -12,16 +13,31 @@ class Deck:
                                              Settings.production))
 
     @classmethod
-    def find(self, key):
+    def deck_title(self, deck):
+        return "Deck id: " + deck.id + " "
+
+    @classmethod
+    def find(self, key, card: bool=False):
         '''
         Find specific deck by key, with key being:
         <id>, <name>, <issuer>, <issue_mode>, <number_of_decimals>
         '''
 
+        #Gather all decks from the node
         decks = pa.find_all_valid_decks(provider,
                                         Settings.deck_version,
                                         Settings.production)
-        return [d for d in decks if key in d.__dict__.values()]
+
+        #Iterate through the list of Deck dicts
+        for d in decks: 
+            if key in d.__dict__.values():
+                #Print deck information as an ASCII table
+                print_table(
+                    title=Deck.deck_title(d),
+                    heading=("asset name", "issuer", "issue mode", "decimals", "issue time"),
+                    data=[[ #Use getattr() to cherrypick key values from dict
+                        getattr(d, attr) for attr in 
+                        ["name", "issuer", "issue_mode", "number_of_decimals", "issue_time"] ]])
 
     @classmethod
     def new(self, name: str, number_of_decimals: int, issue_mode: int,
@@ -36,6 +52,13 @@ class Deck:
 
         pa.deck_spawn(provider, Settings.key, new_deck, Settings.change)
 
+class Card:
+
+    @classmethod
+    def find(self, deck)
+        ''' Find all associated cards '''
+        cards = pa.find_card_transfers(provider, deck)
+        return cards
 
 def main():
 
